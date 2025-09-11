62942a37a819a0c149f0cc5e52b4ecca2e07f285
L30Zh <LEoz47@nycstudents.net>
2025-09-11 10:33:14 -0400

    bob

--- test.py
+++ test.py
@@ -4,17 +4,138 @@ t = Turtle()
 
 
 t.shape('turtle')
+""" 
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(400) """
 
-def square(x)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
 
+""" turtle.done()
+ """
+""" 
+
+
+
+
+
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+equal(300)
+
+turtle.done() """
+
+
+
+
+""" def right(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(135)
+    t.forward((x**2 + x**2)**0.5)
+right(142)
+ """
+""" turtle.done() """
+
+
+
+""" notes: 
+#define the fuction name it then pass arguments/parameters
+def add(a, b):
+    print(x + y)
+add(15,5) """
+
+""" 
+t.speed(15)
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(50)
+square(40)
+square(30)
+square(20)
+square(10)
+
+turtle.done() """
+
+
+
+
+""" sidelength = 250
+rotate = 90
+t.speed(15) # fastest is 15
+
+def bob(x,y,z):
+    for i in range(72):
+        t.left(z)
+        t.forward(x)
+        t.left(y)
+bob(250, 90, 5)
+
+
+
+turtle.done() """
+
+
+
+t.speed (10.5) # 10.5 is fastest
+""" 
+
+def bart(x,y,z):
+    for i in range(18):
+        for bart in range(4):
+            t.forward(x)
+            t.right(y)
+        t.left(z)
+        t.forward(x*1.5)
+        t.right(z*2)
+t.left(190) 
+t.forward(75)
+t.right(90)
+def bill(x,y,z):
+    for i in range(18):
+        for bill in range(4):
+            t.forward(x)
+            t.right(y)
+        t.forward(25)
+t.forward(75)
+def bob(x,y,z):
+    for i in range(6):
+        for bob in range(5):
+            t.forward(x)
+            t.right(y)
+          
+
+
+bart(200,120,20)
+bill(200,85,20)
+bob(150, 75, 15)
 
 turtle.done()
+ """
+
+
+
+
+
+
+
+
